eab7cc5ef342c52995ace93805450afd5331a1f5
HoangNam <huyquang0081@gmail.com>
2023-03-28 21:38:11 +0700

    News

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './component/Header'
 import Footer from './component/Footer';
 import Slide from './component/Slide';
 import TopProduct from './component/Home/TopProduct';
+import News from './component/Home/News';
 export default function App(){
   return (
     <div className='Body'>
       <Header />
       <Slide  />
       <TopProduct />
+      <News />
       <Footer />
     </div>
     );
---
+++ src/component/Home/News.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+export default function News(){
+    return(
+        <div>
+            <h2 style={{color: "White", margin:"20px 20px"}}>Tin tức</h2>
+            <div className="row">
+              <div className="col-md-3" style={{ backgroundImage: "url('https://d2vykwbochoala.cloudfront.net/t_detail/https://d1sdeqoxcfgxl0.cloudfront.net/images/original/f78b65ca69d06e726fba71449c2106a7de1eb520370325b449921c1d.jpg')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+              <div className="col-md-2" style={{ backgroundImage: "url('https://d2vykwbochoala.cloudfront.net/t_detail/https://d1sdeqoxcfgxl0.cloudfront.net/images/original/474891483a7a67e1ff2a9bfa2e9424ee2f5014c7b106f9ba038dcadf.jpg')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+              <div className="col-md-2" style={{ backgroundImage: "url('https://d3gs7vyexhyssp.cloudfront.net/t_detail/https://d1sdeqoxcfgxl0.cloudfront.net/images/original/c01a857f390a002319fd6fe11dede589e721ee46d44b9bed49abe2fd.jpg')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+              <div className="col-md-3" style={{ backgroundImage: "url('https://d2iaiknek2e21s.cloudfront.net/t_detail/https://d1sdeqoxcfgxl0.cloudfront.net/images/original/d668da8bcbb590e3c3254bd5676dc9abb0036d028e6ffabc006f118b.jpg')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-md-3 " style={{ backgroundImage: "url('https://d2vykwbochoala.cloudfront.net/t_detail/https://d1sdeqoxcfgxl0.cloudfront.net/images/original/3d274a5e8dd16544da153f2c5141a1031685236b16facb7275a5e439.jpg')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+              <div className="col-md-4" style={{ backgroundImage: "url('https://d3gs7vyexhyssp.cloudfront.net/t_detail/https://d1sdeqoxcfgxl0.cloudfront.net/images/original/e8842f1cf1b8eb6047b4f3d11933a407f57fdab8bd6edd2dcf3c2c3b.jpg')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+              <div className="col-md-3" style={{ backgroundImage: "url('https://files.porsche.com/filestore/image/multimedia/none/rd-2017-homepage-teaser-ww-tequipmentsound-design-kw27/normal/e19a7537-5cd4-11e7-9717-0019999cd470;sL;twebp/porsche-normal.webp')", height: "300px", backgroundSize: 'cover', position: 'relative', margin: '10px auto'}}>
+              <button style={{ position: 'absolute', bottom: 0, right: 0 , backgroundColor: 'white'}}><i class='bx bxl-facebook-circle' style={{ fontSize: '2em' }}></i></button>
+              </div>
+          </div>
+        </div>
+    )
+}
