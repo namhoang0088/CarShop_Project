4076adfee2d087652615115f67cdb9f1022c59eb
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-04-20 16:25:26 +0700

    init

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@
 // import Authentication from "./Screen/Authentication/Authentication";
 // import Product from "./component/Product/Product.jsx"
 import Payment from './component/Product/Payment';
+import Admin from './Screen/Admin/Admin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   return (
-      <Payment/>
+      <Admin/>
       
   );
 }
