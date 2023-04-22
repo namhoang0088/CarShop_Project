90bc3c1cfb22b41741adc86032d98914b3aa2844
Huynh Nguyen Hieu Nhan <hieunhantv2002@gmail.com>
2023-04-22 16:25:16 +0700

    update black background in product

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import "./App.css";
 
 // import Authentication from "./Screen/Authentication/Authentication";
-// import Product from "./component/Product/Product.jsx"
+import Product from "./component/Product/Product.jsx"
 import Payment from './component/Product/Payment';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App() {
   return (
-      <Payment/>
+      <Product/>
       
   );
 }
