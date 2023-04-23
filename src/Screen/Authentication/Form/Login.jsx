e1b2fea5f0ed87814e7eb92c8fa728ee6a8a7abe
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-04-24 01:57:34 +0700

    add axios to login

--- src/Screen/Authentication/Form/Login.jsx
+++ src/Screen/Authentication/Form/Login.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import "./Login.css";
 import { FiMail } from "react-icons/fi";
 import { FaKey } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 const FormLogin = () => {
+  const [account, setAccount] = useState([]);
+  const [warningEmail, setWarningEmail] = useState([]);
+  useEffect(() => {
+      axios.get('http://localhost/test-react/webcar-ui/BE/Model/Account-data.php')
+        .then(response => setAccount(response.data))
+        .catch(error => console.log(error));
+    }, []);
+    const verifyAccount = (email, password) =>{
+      
+    }
   return (
     <div id="formLogin">
       <form>
@@ -38,8 +49,15 @@ const FormLogin = () => {
             ></input>
           </div>
         </div>
-        <div class="a alert alert-danger border-0 bg-white" role="alert">
-          Sai mật khẩu hoặc enmail!
+        <div class="a alert alert-danger border-0 bg-white" role="alert" style={{color: 'red'}}>
+        <div>
+          <h1>List of accounts:</h1>
+          <ul>
+            {account.map((item, index) => (
+              <li key={index}>{item.name} - {item.email}</li>
+            ))}
+          </ul>
+        </div>
         </div>
         <button type="submit" class="btn btn-primary" id="signIn">
           Sign in
