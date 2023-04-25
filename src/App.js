8cfb00a325834a943bdaf7393dac041873d31b1e
HoangNam <huyquang0081@gmail.com>
2023-04-25 12:34:34 +0700

    fix css cart

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import User from './Screen/User/User';
-import Authentication from './Screen/Authentication/Authentication';
 export default function App() {
   return (
     <div className="Body">
-    <Authentication />
+    <User />
     </div>
   );
 }
